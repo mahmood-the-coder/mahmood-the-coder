1dcbf58c9318bdf6413083fbae99287aa3b68565
Mahmood <candy.codes.co@gmail.com>
2025-03-16 13:04:38 +0330

    removed product

--- components/main-nav/elements/products.js
+++
@@ -1,3 +0,0 @@
-export const products=document.createElement("div");
-products.classList.add("nav__products");
-products.innerText="Products"
--- components/main-nav/index.js
+++ components/main-nav/index.js
@@ -1,11 +1,10 @@
 import { aboutMe } from "./elements/about-me.js";
 import { home } from "./elements/home.js";
-import { products } from "./elements/products.js";
 import { wrapper } from "./elements/wrapper.js";
 
 export function getNav()
 {
-    wrapper.append(home,aboutMe,products)
+    wrapper.append(home,aboutMe)
     return wrapper
 
 }
